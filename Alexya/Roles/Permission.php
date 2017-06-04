3ca9f86a44b23e5ae8e37d404e9e1f3346cc171b
Manulaiko <manulaiko@gmail.com>
2017-06-04 14:16:00 +0200

    Add support for inherited roles.

--- Alexya/Roles/Permission.php
+++ Alexya/Roles/Permission.php
@@ -10,29 +10,43 @@ use Alexya\Tools\Collection;
  * This class represents a permission that can be assigned
  * to a specific role.
  *
- * A permission consists of an identifier and a description.
+ * A permission consists of an identifier, a description and
+ * a status flag.
  *
  * You can easily extend this class to provide more functionality such
  * as alternative names, ranks...
  *
  * @property int    $id          Permission ID.
  * @property string $description Permission description.
+ * @property int    $status      Permission status.
  *
  * @author Manulaiko <manulaiko@gmail.com>
  */
 class Permission extends Collection
 {
+    /////////////////////
+    // Start constants //
+    /////////////////////
+    public const STATUS_INHERITED = 0;
+    public const STATUS_DISABLED  = 1;
+    public const STATUS_ENABLED   = 2;
+    ///////////////////
+    // End constants //
+    ///////////////////
+
     /**
      * Constructor.
      *
      * @param int    $id          Permission ID.
      * @param string $description Permission description.
+     * @param int    $status      Permission status.
      */
-    public function __construct(int $id, string $description)
+    public function __construct(int $id, string $description, int $status)
     {
         parent::__construct([
             "id"          => $id,
-            "description" => $description
+            "description" => $description,
+            "status"      => $status
         ]);
     }
 }
--- Alexya/Roles/Role.php
+++ Alexya/Roles/Role.php
@@ -12,6 +12,7 @@ use Alexya\Tools\Collection;
  * It has an identifier for the role, a description
  * and an array of `Permission` with the granted
  * permissions for this role.
+ * Alternatively, a role can have a parent role for inheritance.
  *
  * The method `hasPermission` accepts an identifier of
  * a permission or an instance of the permission and
@@ -21,6 +22,7 @@ use Alexya\Tools\Collection;
  * @property int          $id          Role identifier.
  * @property string       $description Role description.
  * @property Permission[] $permissions Granted permissions.
+ * @property Role         $parent      Parent role.
  *
  * @author Manulaiko <manulaiko@gmail.com>
  */
@@ -32,13 +34,15 @@ class Role extends Collection
      * @param int          $id          Role identifier.
      * @param string       $description Role description.
      * @param Permission[] $permissions Granted permissions.
+     * @param Role         $parent      Parent role.
      */
-    public function __construct(int $id, string $description, array $permissions)
+    public function __construct(int $id, string $description, array $permissions, Role $parent = null)
     {
         parent::__construct([
-            "id" => $id,
+            "id"          => $id,
             "description" => $description,
-            "permissions" => $permissions
+            "permissions" => $permissions,
+            "parent"      => $parent
         ]);
     }
 }
